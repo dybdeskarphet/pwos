45b58967683f7a075e6a7124f2959c3c2844d86d
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-08-19 17:17:17 +0300

    Make the project accessible as a freestanding binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    todo!();
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
